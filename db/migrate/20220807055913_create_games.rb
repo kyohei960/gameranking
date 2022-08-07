0da6eaa87b0ae902b026c060b28eaf57e5616b30
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-08-07 09:59:44 +0000

    [Ready]カラム記入

--- db/migrate/20220807055913_create_games.rb
+++ db/migrate/20220807055913_create_games.rb
@@ -1,7 +1,10 @@
 class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
-      
+
+      t.integer :game_comments,   null: false
+      t.string  :title,           null: false
+      t.string  :body,            null: false
       t.timestamps
     end
   end
--- db/migrate/20220807060319_create_game_comments.rb
+++ db/migrate/20220807060319_create_game_comments.rb
@@ -2,6 +2,10 @@ class CreateGameComments < ActiveRecord::Migration[6.1]
   def change
     create_table :game_comments do |t|
 
+      t.integer :user_id,              null: false
+      t.integer :game_id,              null: false
+      t.float   :star,                 null: false, default: 0
+      t.string  :game_comment,         null: false
       t.timestamps
     end
   end
