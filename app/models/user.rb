da2e3bbc049ce349ab2dbcd20a190bd29ae6861c
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-08-09 01:39:18 +0000

    [ready]ルーティング直し

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :game_comments
+  has_many :game_comments, dependent: :destroy
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #管理者用url
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
-  } 
+  }
   #ユーザー用url
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "member/registrations",
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   #管理者側のルーティング設定
   namespace :admin do
     root to: "homes#top"
-    resources :games, only: [:new, :index, :show, :create, :destroy, :edit]
+    resources :games, only: [:new, :index, :show, :create, :destroy, :edit] do
+      resources :game_comments, only: [:create, :destroy]
+    end
   end
 end
