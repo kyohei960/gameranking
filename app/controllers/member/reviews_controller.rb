5640d339b802eadd897acc6a930b9e05118c000e
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-08-14 10:02:35 +0000

    レビュー機能途中

--- app/controllers/member/reviews_controller.rb
+++ app/controllers/member/reviews_controller.rb
@@ -1,4 +1,21 @@
 class Member::ReviewsController < ApplicationController
   def index
   end
+  
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect game_reviews_path(@review.game)
+    else
+      @game = Game.find(params[:id])
+      render "games/show"
+    end
+  end
+  
+  private
+  
+  def review_params
+    params.require(:review).permit(:game_id, :score, :content)
+  end
 end
