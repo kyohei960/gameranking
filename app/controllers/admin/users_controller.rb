1ec6836b948b1fc52f2d49b29b9bf40d03eb2952
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-08-19 02:19:24 +0000

    ユーザー管理画面

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,13 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def index
+    @users = User.page(params[:page])
+  end
+  
+  def destroy
+    @user = Game.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
 end
