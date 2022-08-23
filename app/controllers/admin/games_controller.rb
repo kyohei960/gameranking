86a5449c98c0f1e8cd98681e922bc2827fc2b4bd
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-08-23 15:48:55 +0000

    フッター修正

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -17,7 +17,31 @@ class Admin::GamesController < ApplicationController
   end
 
   def index
-    @games = Game.page(params[:page])
+    #タグで絞り込まれた場合
+    if params[:tag_id].present?
+      @tag = Tag.find(params[:tag_id])
+      @games = @tag.games.order(created_at: :desc)
+    #ジャンルで検索された場合
+    elsif params[:genre] != nil
+      @games = Game.where(genre_id: params[:genre])
+    else
+      #評価が高い順に並べるようにするため
+      @games = Game.
+              left_joins(:reviews).
+              distinct.
+              sort_by do |game|
+                hoges = game.reviews
+                if hoges.present?
+                  hoges.map(&:score).sum / hoges.size
+                else
+                  0
+                end
+              end.
+              reverse
+    end
+    @tag_lists = Tag.all
+    @games = Kaminari.paginate_array(@games).page(params[:page]).per(9)
+  end
   end
 
   def show
--- app/controllers/member/games_controller.rb
+++ app/controllers/member/games_controller.rb
@@ -1,12 +1,15 @@
 class Member::GamesController < ApplicationController
   before_action :authenticate_user!, only: [:show]
   def index
+    #タグで絞り込まれた場合
     if params[:tag_id].present?
       @tag = Tag.find(params[:tag_id])
       @games = @tag.games.order(created_at: :desc)
+    #ジャンルで検索された場合
     elsif params[:genre] != nil
       @games = Game.where(genre_id: params[:genre])
     else
+      #評価が高い順に並べるようにするため
       @games = Game.
               left_joins(:reviews).
               distinct.
